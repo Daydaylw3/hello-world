fb0cbfae199ffe48a3187891633be44a6e74939f
daydaytan <daydaytan@tencent.com>
2022-04-22 17:11:57 +0800

    arr_ins_del.c: 演示了数组元素的插入和删除操作
    variable_len_arr.c: 变长数组(VLA, Variable Length Array)
    sleep.c: 预处理命令: 区分win和linux进行编译执行

---
+++ c/#/sleep.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#if _WIN32
+#include <windows.h>
+#elif __linux__
+#include <unistd.h>
+#endif
+
+int main() {
+	#if _WIN32
+	Sleep(5000);
+	#elif __linux__
+	sleep(5);
+	#endif
+	
+	puts("end..");
+	
+	return 0;
+}
